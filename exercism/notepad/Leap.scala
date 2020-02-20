422fa238d5526d5f14988f0e5160c9be48887232
DUVELA <rldudcjsdla@gmail.com>
2020-02-20 11:26:46 +0900

    Delete Leap.scala

--- exercism/notepad/Leap.scala
+++
@@ -1,8 +0,0 @@
-object Leap {
-  def leapYear(year: Int): Boolean = year match {
-    case x if (x % 4 == 0) && (x % 400 != 0) => false
-    case x if (x % 4 == 0) => true
-    case _ => false
-  }
-}
-    
