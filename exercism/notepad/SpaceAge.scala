e08f4b594eae910998fd7c3d34cdebde26c50abf
DUVELA <rldudcjsdla@gmail.com>
2020-02-20 20:40:52 +0900

    Update SpaceAge.scala

--- exercism/notepad/SpaceAge.scala
+++ exercism/notepad/SpaceAge.scala
@@ -14,16 +14,16 @@ object SpaceAge {
         )
 
     /* ====== back story()()() start */
-    def onEarth(x : Double) = x / ddt(onSecEarthTime)("Earth")
-    def onMercury(x: Double) = x / ddt(onSecEarthTime)("Mercury")
-    def onVenus(x: Double) = x / ddt(onSecEarthTime)("Venus")
-    def onMars(x: Double) = x / ddt(onSecEarthTime)("Mars")
-    def onJupiter(x: Double) = x / ddt(onSecEarthTime)("Jupiter")
-    def onSaturn(x: Double) = x / ddt(onSecEarthTime)("Saturn")
-    def onUranus(x: Double) = x / ddt(onSecEarthTime)("Uranus")
-    def onNeptune(x: Double) = x / ddt(onSecEarthTime)("Neptune")
+    def onEarth(x : Double) = x / ddt()("Earth")
+    def onMercury(x: Double) = x / ddt()("Mercury")
+    def onVenus(x: Double) = x / ddt()("Venus")
+    def onMars(x: Double) = x / ddt()("Mars")
+    def onJupiter(x: Double) = x / ddt()("Jupiter")
+    def onSaturn(x: Double) = x / ddt()("Saturn")
+    def onUranus(x: Double) = x / ddt()("Uranus")
+    def onNeptune(x: Double) = x / ddt()("Neptune")
     /* ====== back story end */
 
-    def ddt(acc: Double)(y: String) = onSecEarthTime * howOldRU(y)
+    def ddt(acc: Double = onSecEarthTime)(y: String) = onSecEarthTime * howOldRU(y)
     // y = () => ???
 }
