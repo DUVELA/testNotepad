1be05f185f7cb72f67a8e7b5bfbc0bf5f5e5b156
DUVELA <rldudcjsdla@gmail.com>
2020-02-21 18:50:03 +0900

    Update Leap.scala

--- exercism/notepad/Leap.scala
+++ exercism/notepad/Leap.scala
@@ -1,7 +1,7 @@
 object Leap {
   def leapYear(year: Int): Boolean = year match {
-    case x if (x % 4 == 0) && (x % 400 != 0) => false
-    case x if (x % 4 == 0) => true
+    case x if (x % 400 == 0) => true
+    case x if (x % 4 == 0)  && (x % 100 != 0) => true
     case _ => false
   }
 }
