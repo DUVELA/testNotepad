be3a3aa2b32cc0b17906ad78b73aaf5e46d14801
DUVELA <rldudcjsdla@gmail.com>
2020-08-25 16:27:19 +0900

    Update upperlowercase.scala

--- kNote/upperlowercase.scala
+++ kNote/upperlowercase.scala
@@ -1,20 +1,20 @@
 object Hello {
   val testCase = "path of exile".toUpperCase
 
-  //띄어 쓰기로 나눔 시작
+  //Divide by space start
   def stringTest(s: String) = {
     for (a <- s.split(" "))
     yield a
   }
-  //띄어 쓰기로 나눔 끝
+  //Divide by space end
   
   val ddt = stringTest(testCase)
   
-  // 각 대,소문자 반복 시작
+  // repeating upper and lower start
   def ddtTest(a: Array[String]) = {
     for(i <- 0 until ddt.size)
     yield for(j <- 0 until ddt(i).size)
     yield if(j % 2 == 1) ddt(i)(j).toString.toLowerCase else ddt(i)(j)
   }
-  // 각 대,소문자 반복 끝
+  // repeating upper and lower end
 }
