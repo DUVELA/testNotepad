42b671b0a4dfb7158a9775e4827b509f16f4e400
DUVELA <rldudcjsdla@gmail.com>
2020-09-01 22:56:02 +0900

    Create uniqueElement.scala
    Set과 다른? 중복이 아닌 값.

---
+++ magicBox/uniqueElement.scala
@@ -0,0 +1,8 @@
+object Hello {
+  def findUnique(xs: List[List[Int]]): List[Int] = {
+        val x = xs.flatten
+        x.filter(i => x.indexOf(i) == x.lastIndexOf(i))
+    }
+}
+
+//https://stackoverflow.com/questions/1538598/how-in-scala-to-find-unique-items-in-list
