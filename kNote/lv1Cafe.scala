997a9ac502ac2dec07b92a861b472350f823d66c
DUVELA <rldudcjsdla@gmail.com>
2020-09-03 21:48:07 +0900

    Delete lv1Cafe.scala

--- kNote/lv1Cafe.scala
+++
@@ -1,9 +0,0 @@
-import scala.math.{sqrt, floor}
-
-object roundCafe {
-    def roundColor(x: Int, y: Int):Vector[Int] = {
-        implicit val c = (sqrt(y).floor +2).toInt
-        implicit val r = (x + y) / c
-        Vector(r, c)
-    }
-}
