a5e59cecc1140b9a8de64437c63fbb149f29a9e1
DUVELA <rldudcjsdla@gmail.com>
2020-09-03 21:23:50 +0900

    Update uniqueElement.scala
    이번엔 조건에 맞게 다 풀어놓지 않고 하는걸 찾은듯 하다.

--- magicBox/uniqueElement.scala
+++ magicBox/uniqueElement.scala
@@ -3,6 +3,12 @@ object Hello {
         val x = xs.flatten
         x.filter(i => x.indexOf(i) == x.lastIndexOf(i))
     }
+  
+  def hiddenPoint(r: List[List[Int]]) = {
+        val rHead = (for(i <- 0 until r.size) yield r(i).head).toList
+        val rLast = (for(i <- 0 until r.size) yield r(i).last).toList
+        (rHead.filter{a => rHead.indexOf(a) == rHead.lastIndexOf(a)} ::: rLast.filter{a => rLast.indexOf(a) == rLast.lastIndexOf(a)})
+    }
 }
 
 //https://stackoverflow.com/questions/1538598/how-in-scala-to-find-unique-items-in-list
