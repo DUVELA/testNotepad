da43ae62921edb45bbd4b0aac4c7c4273ea197c7
DUVELA <rldudcjsdla@gmail.com>
2021-01-03 09:46:24 +0900

    Revert "Create DLFSfunction.scala"
    This reverts commit 42f3693cffcd08bb6c2d4045ebc687ed5954effa.

--- magicBox/DLFSfunction.scala
+++
@@ -1,7 +0,0 @@
-import scala.math.exp
-
-object pythonFunctionNotepad {
-    def identity_function[A](x: A): A = x   // .\.metals\readonly\scala\Predef.scala
-
-    def sigmoid(x: Double): Double = 1 / (1 + exp(-x))
-}
