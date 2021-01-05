fa1ea17ad57cb480850172755a35ead54151ba99
DUVELA <rldudcjsdla@gmail.com>
2021-01-05 21:53:40 +0900

    Update DLFSfunction.scala
    #1 py에서 relu_grad(2.5)를 했을 때  >>> relu_grad(2.5) Traceback (most recent call last): \n  File "<stdin>", line 1, in <module>\n   File "<stdin>", line 2, in relu_grad  가 나와서 Int로 해도 되겠다 싶어서.

--- magicBox/DLFSfunction.scala
+++ magicBox/DLFSfunction.scala
@@ -4,4 +4,22 @@ object pythonFunctionNotepad {
     def identity_function[A](x: A): A = x   //identity 가 그냥 있긴 하다.  .\.metals\readonly\scala\Predef.scala
 
     def sigmoid(x: Double): Double = 1 / (1 + exp(-x))
+
+    def oneOrZero(x: Double): Double = if(x > 0 ) 1 else 0
+
+    def checkTwoType[A](x: A) = x match {
+        case x: Double => oneOrZero(x)
+        case x: List[Double] => x.map(oneOrZero(_))
+    }
+
+    def sigmoid_grad(x: Double) = (1.0 - sigmoid(x)) * sigmoid(x)
+
+    def max(a: Double = 0, b: Double) = if(a > b) a else b
+    def relu[A](x: A) = x match {
+        case x: Double => max(0, x)
+        case x: List[Double] => x.map(max(0, _))
+    }
+
+    def relu_grade(x: Int): List[Int] = List.fill(x)(1) //#1
+
 }
