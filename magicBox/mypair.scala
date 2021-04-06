ea5bc0f7761698a810c8854fe0986e02af766517
DUVELA <rldudcjsdla@gmail.com>
2021-04-06 22:49:53 +0900

    Create mypair.scala
    원하던 튜플인 값을 넣었을 땐 안됨. 방법은 모르겠다.

---
+++ magicBox/mypair.scala
@@ -0,0 +1,12 @@
+def fst[A, B](x: A, y: B): A = x
+
+def snd[A, B](x:A, y: B): B = y
+
+// mypair.idris
+/*
+scala> def fst[A, B](x: A, y: B): A = x
+fst: [A, B](x: A, y: B)A
+
+scala> def snd[A, B](x:A, y: B): B = y
+snd: [A, B](x: A, y: B)B
+*/
