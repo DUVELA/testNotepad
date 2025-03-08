51fcde136d047da2dadf5d94892e831722ce9f1a
DUVELA <rldudcjsdla@gmail.com>
2025-03-08 19:17:08 +0900

    Update babbling.scala
    '조건이 최대 한 번씩만 등장합니다.'라고 함. -ayamaaya 는 0 이어야 하는듯.-

--- magicBox/babbling.scala
+++ magicBox/babbling.scala
@@ -1,7 +1,7 @@
 import scala.util.matching.Regex
 
 object Solution {
-    val patterns = "^(aya|ye|woo|ma)+$".r
+    val patterns = "^(aya(?!.*aya)|ye(?!.*ye)|woo(?!.*woo)|ma(?!.*ma))+$".r
     def solution(babbling: Vector[String]): Int = {
         babbling.map(i => patterns.matches(i)).count(_ == true)
     }
