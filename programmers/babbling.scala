1b4b01fb3272c9ed6125745846f2d672f78986b7
DUVELA <rldudcjsdla@gmail.com>
2025-03-19 21:23:29 +0900

    Update babbling.scala
    copilot의 도움을 받음.

--- programmers/babbling.scala
+++ programmers/babbling.scala
@@ -5,7 +5,7 @@ import scala.util.matching.Regex
 
 object Main {
     def main(args: Array[String]): Unit = {
-        val inputFileName = "programmers/input.txt"
+        val inputFileName = "programmers/input_babbling.txt"
         val lines = Source.fromFile(inputFileName).getLines().toList
         
         //첫 번째 줄에서 테스트 케이스의 개수
@@ -15,20 +15,17 @@ object Main {
         val patterns = "^(aya(?!.*aya)|ye(?!.*ye)|woo(?!.*woo)|ma(?!.*ma))+$".r
         def solution(babbling: List[String]): Int = {
             babbling.map(i => {
-                // println(s"Checking word: $i")
                 patterns.matches(i)
             }).count(_ == true)
         }
         val results = lines.tail.zipWithIndex.map{case (lineString, index) =>
             val bab = lineString.drop(1).dropRight(1).split(", ").map(_.replace("\"", "")).toList
-            // println(s"Processed bab: ${bab(0)}")
-            // println(s"Case #${index + 1}: ${solution(bab)}")
             s"Case #${index + 1}: ${solution(bab)}"
         }
         //작업 끝
 
         //output생성
-        val outputFileName = "programmers/output.txt"
+        val outputFileName = "programmers/output_babbling.txt"
         val writer = new PrintWriter(new File(outputFileName))
         results.foreach{result =>
             println(result)
@@ -37,9 +34,3 @@ object Main {
         writer.close()
     }
 }
-
-/**
- * val groupingSymbol = Map("[" -> "(", "]" -> ")")
- * val groupingRex = """[\[\]]""".r
- * val x = """["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]"""
-*/
