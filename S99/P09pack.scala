209f70e26e9e4b1e25708080ce6fc956404fafd6
DUVELA <rldudcjsdla@gmail.com>
2025-04-01 03:05:07 +0900

    Delete S99/P09pack.scala

--- S99/P09pack.scala
+++
@@ -1,9 +0,0 @@
-def pack[A](ls: List[A]) = {
-    if(ls.isEmpty) List(List())
-    else ls.foldLeft(List.empty[List[A]]){
-        case (Nil, Nil) => List(List())
-        case (Nil, a) => List(List(a))
-        case (l, a) if(l.head.contains(a)) => List(a :: l.head) ++ l.tail
-        case (l, a) => List(List(a)) ++ l
-    }.reverse
-}
