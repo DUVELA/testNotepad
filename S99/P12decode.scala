07faa6713de4af1c930e906d4951c4f597caaa86
DUVELA <rldudcjsdla@gmail.com>
2025-04-03 01:23:51 +0900

    Update P12decode.scala
    P12의 solution을 보니 make를 쓰는데 copilot한테 물어보니 make는 더 이상 지원하지 않는다고 하면서 내 코드롤 개선해 줌.
    """
    def decode[A](ls: List[(Int, A)]): List[A] =
      ls flatMap { case (count, value) => List.fill(count)(value) }
    
    """
    - 왜 flaMap에서 flatten을 하지 않았는데 결과물이 같은 거야?
    "flatMap은 map과 flatten을 한 번에 수행하는 함수입니다."
    "flatMap은 먼저 각 요소에 대해 변환 함수를 적용해 중첩된 결과(List[List[A]])를 만들고, 이를 자동으로 평평하게(Flatten) 처리하여 최종 결과를 반환합니다."

--- S99/P12decode.scala
+++ S99/P12decode.scala
@@ -1,1 +1,3 @@
-def decode[A](ls: List[(Int, A)]): List[A] = ls.map(t => List.fill(t._1)(t._2)).flatten
+// def decode[A](ls: List[(Int, A)]): List[A] = ls.map(t => List.fill(t._1)(t._2)).flatten
+
+def decode[A](ls: List[(Int, A)]): List[A] = ls flatMap{ e => List.fill(e._1)(e._2)}
